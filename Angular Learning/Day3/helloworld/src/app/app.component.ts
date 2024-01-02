ecf1ef536a3d0250e6df3dcea1ec56542f302e83
Shimbhu77 <kumawatshimbhu4@gmail.com>
2024-01-02 23:07:13 +0530

    composing components means how to use components in the application

--- Angular Learning/Day3/helloworld/src/app/app.component.ts
+++ Angular Learning/Day3/helloworld/src/app/app.component.ts
@@ -2,16 +2,27 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+
+
+@Component({
+  selector:'app-user',
+  standalone:true,
+  template:'Username : {{name}}',
+  styles:`:host{ 
+    color:blue;
+  }`
+})
+
+export class UserComponent{
+  name = "Shimbhu";
+}
+
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  template:`Hello {{name}}!`,
-  styles:`
-  :host{
-    color:purple;
-  }
-  `
+  template:`<app-user>`,
+  imports:[UserComponent],
 })
-export class AppComponent {
-  name = "Shimbhu";
-}
+export class AppComponent {}
