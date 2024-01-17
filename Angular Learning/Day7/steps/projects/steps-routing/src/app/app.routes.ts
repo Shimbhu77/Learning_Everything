443124a06af4d995914de443700e847c481f80b2
Shimbhu77 <kumawatshimbhu4@gmail.com>
2024-01-17 21:36:47 +0530

    page not found or wild card routing

--- Angular Learning/Day7/steps/projects/steps-routing/src/app/app.routes.ts
+++ Angular Learning/Day7/steps/projects/steps-routing/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { FirstComponent } from './first/first.component';
 import { SecondComponent } from './second/second.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 export const routes: Routes = [
     {path:'first-component',component:FirstComponent},
-    {path:'second-component',component:SecondComponent}
+    {path:'second-component',component:SecondComponent},
+    {path:'**',component:PagenotfoundComponent}
 ];
