f4c011ced613a9afe45241c7b21d12ebf7c7476e
Shimbhu77 <kumawatshimbhu4@gmail.com>
2024-01-18 20:13:43 +0530

    route redirection implemented

--- Angular Learning/Day7/steps/projects/steps-routing/src/app/app.routes.ts
+++ Angular Learning/Day7/steps/projects/steps-routing/src/app/app.routes.ts
@@ -6,5 +6,6 @@ import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 export const routes: Routes = [
     {path:'first-component',component:FirstComponent},
     {path:'second-component',component:SecondComponent},
+    {path:'',redirectTo:'/first-component',pathMatch:'full'},
     {path:'**',component:PagenotfoundComponent}
 ];
