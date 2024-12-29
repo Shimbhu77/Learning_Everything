1ac9a2b51d72026e370646dc2030ebec0e991bb3
Shimbhu77 <kumawatshimbhu4@gmail.com>
2024-12-29 10:16:49 +0530

    Implemented the setter injection using Spring IOC

--- Spring Revision/SpringCoreRevision/src/main/java/com/shimbhu/setter_injection/Student.java
+++ Spring Revision/SpringCoreRevision/src/main/java/com/shimbhu/setter_injection/Student.java
@@ -1,12 +1,8 @@
 package com.shimbhu.setter_injection;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Student {
 
     private Integer studentId;
