ea76fe5a6ee4617708c243f4ef900c73ad393907
Shimbhu77 <kumawatshimbhu4@gmail.com>
2024-12-29 15:07:39 +0530

    Autowiring By Name Using Setter Method

---
+++ Spring Revision/SpringCoreRevision/src/main/java/com/shimbhu/auto_wiring/setter_injection/Fort.java
@@ -0,0 +1,10 @@
+package com.shimbhu.auto_wiring.setter_injection;
+
+
+import lombok.Data;
+
+@Data
+public class Fort {
+    private String name;
+    private Location location;
+}
---
+++ Spring Revision/SpringCoreRevision/src/main/java/com/shimbhu/auto_wiring/setter_injection/Location.java
@@ -0,0 +1,9 @@
+package com.shimbhu.auto_wiring.setter_injection;
+
+import lombok.Data;
+
+@Data
+public class Location {
+    private String city;
+    private String state;
+}
